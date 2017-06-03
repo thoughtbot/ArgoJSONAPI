d6bc13f0d0032a71ea639f3f25378fb8bf3599cc
Adam Sharp <adam@sharplet.me>
2017-06-03 01:23:33 -0400

    Remove duplication caused by Document enum cases
    By dynamically modelling resources and collections as enum cases, we
    lose the ability to statically determine whether we expect to decode a
    single resource or a collection, and so we have to dynamically
    destructure the document in order to restore the state guarantee.
    
    Removing the enum cases and statically calling `decodeResource` or
    `decodeCollection` gives all the safety we need and removes duplication.

--- Sources/ArgoJSONAPI/Document.swift
+++ Sources/ArgoJSONAPI/Document.swift
@@ -1,45 +1,28 @@
 import Argo
 import Runes
 
-extension JSONAPI {
-  public enum Document<Resource: JSONAPIDecodable>: Argo.Decodable {
-    case resource(Resource.DecodedType)
-    case collection([Resource.DecodedType])
+enum Document<Resource: JSONAPIDecodable> {
+  static func decodeCollection(_ data: JSON) -> Decoded<[Resource.DecodedType]> {
+    let array: Decoded<[JSON]> = decodeArray(data)
 
-    public static func decode(_ json: JSON) -> Decoded<Document> {
-      let data: Decoded<JSON> = json <| "data"
+    return array >>- { array in
+      sequence(decodeResource <^> array)
+    }
+  }
 
-      let collection = Document.collection
-        <^> data.flatMap(decodeCollection)
+  static func decodeResource(_ data: JSON) -> Decoded<Resource.DecodedType> {
+    let type: Decoded<String> = data <| "type"
 
-      let resource = Document.resource
-        <^> data.flatMap(decodeResource)
-
-      return collection <|> resource
+    guard type.value == Resource.resourceType else {
+      return .typeMismatch(expected: Resource.resourceType, actual: data)
     }
 
-    static func decodeCollection(_ data: JSON) -> Decoded<[Resource.DecodedType]> {
-      let array: Decoded<[JSON]> = decodeArray(data)
+    let id: Decoded<String> = data <| "id"
+    let attributes: Decoded<JSON?> = data <|? "attributes"
 
-      return array >>- { array in
-        sequence(decodeResource <^> array)
-      }
-    }
-
-    static func decodeResource(_ data: JSON) -> Decoded<Resource.DecodedType> {
-      let type: Decoded<String> = data <| "type"
-
-      guard type.value == Resource.resourceType else {
-        return .typeMismatch(expected: Resource.resourceType, actual: data)
-      }
-
-      let id: Decoded<String> = data <| "id"
-      let attributes: Decoded<JSON?> = data <|? "attributes"
-
-      return id >>- { id in
-        attributes >>- { attributes in
-          Resource.decode(id: id, attributes: attributes ?? .object([:]))
-        }
+    return id >>- { id in
+      attributes >>- { attributes in
+        Resource.decode(id: id, attributes: attributes ?? .object([:]))
       }
     }
   }
