6cf1367e86d2f813edf846bc547aff43818fc771
Adam Sharp <adam@sharplet.me>
2017-06-04 15:10:55 -0400

    Remove inaccurate use of "simple" in naming

--- Sources/ArgoJSONAPI/Document.swift
+++ Sources/ArgoJSONAPI/Document.swift
@@ -4,15 +4,15 @@ import Runes
 enum Document<Resource: JSONAPIDecodable> {
   static func decodeCollection(_ json: JSON) -> Decoded<[Resource.DecodedType]> {
     return JSONAPI.Data.decodeCollection(json) >>- { collection in
-      sequence(decodeSimpleResource <^> collection)
+      sequence(decode <^> collection)
     }
   }
 
   static func decodeResource(_ json: JSON) -> Decoded<Resource.DecodedType> {
-    return JSONAPI.Data.decodeResource(json) >>- decodeSimpleResource
+    return JSONAPI.Data.decodeResource(json) >>- decode
   }
 
-  private static func decodeSimpleResource(_ data: JSONAPI.Data) -> Decoded<Resource.DecodedType> {
+  private static func decode(_ data: JSONAPI.Data) -> Decoded<Resource.DecodedType> {
     guard data.type == Resource.resourceType else {
       return .typeMismatch(expected: Resource.resourceType, actual: data)
     }
