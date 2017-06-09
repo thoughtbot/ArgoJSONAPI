26bb44d6ce16d798701dc66dfc60b769660f407e
Adam Sharp <adam@sharplet.me>
2017-06-09 09:08:11 -0400

    Update to Nimble 7.0.1

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     .package(url: "https://github.com/thoughtbot/Argo.git", from: "4.0.0"),
     .package(url: "https://github.com/thoughtbot/Curry.git", from: "3.0.0"),
     .package(url: "https://github.com/Quick/Quick.git", .branch("as-swift-4-swift-3-compat")),
-    .package(url: "https://github.com/Quick/Nimble.git", .branch("master")),
+    .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.1"),
   ],
   targets: [
     .target(
